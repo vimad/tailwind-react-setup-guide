95223b2617affa39a1e1df8d6a2ecb8c42f51a45
vinod.mby@gmail.com <vinod madubashana>
2024-01-07 15:03:00 +0530

    setup tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>This is Tailwind CSS setup guide</h1>;
+  return (
+    <h1 className="text-4xl text-center font-semibold mt-4 text-green-600">
+      This is Tailwind CSS setup guide
+    </h1>
+  );
 }
 
 export default App;
