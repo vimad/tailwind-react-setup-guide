880c3b0df48eb48c100ac35c609c913897d1c8f2
vinod.mby@gmail.com <vinod madubashana>
2024-01-12 13:22:36 +0530

    useState hook example

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0); // ------------------> 1
+  // const [count, setCount] = useState(() => 0);  // --------> 2
+
+  let count2 = 1; // -----------------------------------------> 3
+  function handleIncrease() {
+    setCount((count) => count + 1); // -----------------------> 4
+    // setCount(count + 1); // -------------------------------> 5
+    count2 += 1; // ------------------------------------------> 6
+  }
+
   return (
-    <h1 className="mt-4 text-center text-5xl font-semibold text-green-600">
-      This is Tailwind CSS setup guide
-    </h1>
+    <div className="mt-5 flex flex-col items-center justify-center">
+      <button
+        onClick={handleIncrease}
+        className=" w-40 rounded-full bg-orange-300  px-2 py-4"
+      >
+        Increase Count
+      </button>
+      <div className="mt-2">
+        {count} - {count2}
+      </div>
+    </div>
   );
 }
 
